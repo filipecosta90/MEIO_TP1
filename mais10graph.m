6875d26fb672317bce50c6f9b776c9f2cd1e9bc7
filipecosta90 <filipecosta.90@gmail.com>
2016-04-07 19:16:59 +0100

    added all files necessary to the report production

---
+++ mais10graph.m
@@ -0,0 +1,27 @@
+  FigHandle = figure;
+  set(FigHandle, 'Position', [0, 0, 640, 640]);
+
+mais10 = csvread('10compras.csv');
+
+
+nservidores = mais10 ( :, [1]); 
+wq = mais10 ( : ,[6]);
+figure(1)
+p = plot(nservidores,wq)
+hline = refline([0 90]);
+hline.Color = 'r';
+p(1).LineWidth = 3;
+
+l = legend('Wq, #unidades <= 10' ,'limite max. permitido (90s)');
+
+set(l,'FontSize',12);
+
+ylabel('Wq -Tempo  m\''{e}dio de espera na fila (s)' ,'interpreter','latex');
+
+xlabel('#Servidores');
+
+set(gca,'fontsize',12);
+set(gca,'XTick',nservidores)
+title({'An\''{a}lise gr\''{a}fica da evolu\c{c}\~{a}o do tempo m\''{e}dio de espera na fila,' 'mediante o aumento do n\''{u}mero de servidores envolvidos,' 'para clientes com n\''{u}mero de unidades de artigos inferior ou igual a 10'},'interpreter','latex');
+
+
